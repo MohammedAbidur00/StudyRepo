7adf2988a78053940a039e30830bb8cddeead215
MohammedAbidur00 <abidurrahman788909@gmail.com>
2026-05-26 18:17:47 +0100

    feat: created login and registration page UIs, also installed express

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,24 @@
 const headerCurrentUserLabelContainer = document.getElementById("headerCurrentUserLabelContainer");
+const loginPageWrapper = document.getElementById("loginPageWrapper");
+const registrationPageWrapper = document.getElementById("registrationPageWrapper");
+const goToRegisterBtn = document.getElementById("goToRegisterBtn");
+const goToLoginBtn = document.getElementById("goToLoginBtn");
 
 let currentUser = "Harry990";
 
+goToRegisterBtn.addEventListener("click", () => {
+    loginPageWrapper.classList.add("Hide");
+    registrationPageWrapper.classList.remove("Hide");
+})
+
+goToLoginBtn.addEventListener("click", () => {
+    registrationPageWrapper.classList.add("hide");
+    loginPageWrapper.classList.remove("Hide");
+})
+
 function showCurrentUser(currentUser) {
     let currentUserBtn = document.createElement("button");
-    currentUserBtn.textContent = `USER: ${currentUser}`;
+    currentUserBtn.textContent = currentUser;
     currentUserBtn.classList.add("Header-Label-Btn");
     headerCurrentUserLabelContainer.appendChild(currentUserBtn);
 }
